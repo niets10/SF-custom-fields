56c1b20d08bdf27f9b567ab5cfca94ad1337f934
Álvaro Nieto <agnieto00@gmail.com>
2021-02-08 19:34:56 +0100

    Added mapping for all the fields

--- utilities/metadata-mapping.js
+++ utilities/metadata-mapping.js
@@ -1,7 +1,14 @@
+function generateMetadata(field) {
+
+    let metadata;
+
+    
+
+
+    return metadata;
+}
+
 function autoNumberMetadata(field, objectName){
-
-    console.log('Field ' + JSON.stringify(field));
-
     let metadata = {
         type: field.type,
         label : field.label,
@@ -12,11 +19,230 @@ function autoNumberMetadata(field, objectName){
         startingNumber : field.startingNumber,
         displayFormat : field.displayFormat,
     } 
-
-    console.log('Metadata ' + JSON.stringify(metadata));
-
     return metadata;
-
 }
 
-module.exports = { autoNumberMetadata };
+function checkboxMetadata(field, objectName){
+    let metadata = {
+        type: field.type,
+        label : field.label,
+        fullName : objectName+"."+field.apiName,
+        description: field.description,
+        inlineHelpText : field.helpText,
+        defaultValue : field.defaultValue
+    }
+    return metadata;
+}
+function currencyMetadata(field, objectName){
+    let metadata = {
+        type: field.type,
+        label : field.label,
+        fullName : objectName+"."+field.apiName,            
+        length: field.length,
+        scale: field.decimalPlaces,
+        description: field.description,
+        inlineHelpText : field.helpText,
+        required : field.required
+    }
+    return metadata;
+}
+
+function currencyMetadata(field, objectName){
+    let metadata = {
+        type: field.type,
+        label : field.label,
+        fullName : objectName+"."+field.apiName,            
+        length: field.length,
+        scale: field.decimalPlaces,
+        description: field.description,
+        inlineHelpText : field.helpText,
+        required : field.required
+    }
+    return metadata;
+}
+
+function dataDataTimeMetadata(field, objectName){
+    let metadata = {
+        type: field.type,
+        label : field.label,
+        fullName : objectName+"."+field.apiName,
+        description: field.description,
+        inlineHelpText : field.helpText,
+        required : field.required
+    }
+    return metadata;
+}
+
+function emailMetadata(field, objectName){
+    let metadata = {
+        type: field.type,
+        label : field.label,
+        fullName : objectName+"."+field.apiName,
+        description: field.description,
+        inlineHelpText : field.helpText,
+        required : field.required,
+        unique: field.unique,
+        externalId : field.externalId
+    }
+    return metadata;
+}
+
+function locationMetadata(field, objectName){
+
+    let displayDecimals;
+    if(field.displayFormat === 'Decimals'){
+        displayDecimals = true;
+    }else if(field.displayFormat === 'Degrees, minutes, seconds'){
+        displayDecimals = false;
+    }
+
+    let metadata = {
+        type: field.type,
+        label : field.label,
+        fullName : objectName+"."+field.apiName,
+        scale: field.decimalPlaces,
+        description: field.description,
+        inlineHelpText : field.helpText,
+        required : field.required,
+        displayLocationInDecimal: displayDecimals
+    }
+    return metadata;
+}
+
+function numberMetadata(field, objectName){
+    let metadata = {
+        type: field.type,
+        label : field.label,
+        fullName : objectName+"."+field.apiName,            
+        length: field.length,
+        scale: field.decimalPlaces,
+        description: field.description,
+        inlineHelpText : field.helpText,
+        required : field.required,
+        unique: field.unique,
+        externalId : field.externalId
+    }
+    return metadata;
+}
+function percentMetadata(field, objectName){
+    let metadata = {
+        type: field.type,
+        label : field.label,
+        fullName : objectName+"."+field.apiName,            
+        length: field.length,
+        scale: field.decimalPlaces,
+        description: field.description,
+        inlineHelpText : field.helpText,
+        required : field.required
+    }
+    return metadata;
+}
+function phoneMetadata(field, objectName){
+    let metadata = {
+        type: field.type,
+        label : field.label,
+        fullName : objectName+"."+field.apiName,
+        description: field.description,
+        inlineHelpText : field.helpText,
+        required : field.required
+    }
+    return metadata;
+}
+function picklistMetadata(field, objectName){
+    let metadata = {
+        type: field.type,
+        label : field.label,
+        fullName : objectName+"."+field.apiName,
+        description: field.description,
+        inlineHelpText : field.helpText,
+        required : field.required,
+        valueSet: field.picklistValues
+    }
+    return metadata;
+}
+function multiPicklistMetadata(field, objectName){
+    let metadata = {
+        type: field.type,
+        label : field.label,
+        fullName : objectName+"."+field.apiName,
+        description: field.description,
+        inlineHelpText : field.helpText,
+        required : field.required,
+        valueSet: field.picklistValues,
+        visibleLines : field.visibleLines
+    }
+    return metadata;
+}
+
+function textMetadata(field, objectName){
+    let metadata = {
+        type: field.type,
+        label : field.label,
+        fullName : objectName+"."+field.apiName,            
+        length: field.length,
+        description: field.description,
+        inlineHelpText : field.helpText,
+        required : field.required,
+        unique: field.unique,
+        externalId : field.externalId,
+    }
+    return metadata;
+}
+function textAreaMetadata(field, objectName){
+    let metadata = {
+        type: field.type,
+        label : field.label,
+        fullName : objectName+"."+field.apiName,
+        description: field.description,
+        inlineHelpText : field.helpText,
+        required : field.required
+    }
+    return metadata;
+}
+function longTextAreaMetadata(field, objectName){
+    let metadata = {
+        type: field.type,
+        label : field.label,
+        fullName : objectName+"."+field.apiName,            
+        length: field.length,
+        description: field.description,
+        inlineHelpText : field.helpText
+    }
+    return metadata;
+}
+function richTextAreaMetadata(field, objectName){
+    let metadata = {
+        type: field.type,
+        label : field.label,
+        fullName : objectName+"."+field.apiName,            
+        length: field.length,
+        description: field.description,
+        inlineHelpText : field.helpText,
+        visibleLines : field.visibleLines,
+    }
+    return metadata;
+}
+function timeMetadata(field, objectName){
+    let metadata = {
+        type: field.type,
+        label : field.label,
+        fullName : objectName+"."+field.apiName,
+        description: field.description,
+        inlineHelpText : field.helpText,
+        required : field.required
+    }
+    return metadata;
+}
+function urlMetadata(field, objectName){
+    let metadata = {
+        type: field.type,
+        label : field.label,
+        fullName : objectName+"."+field.apiName,
+        description: field.description,
+        inlineHelpText : field.helpText,
+        required : field.required
+    }
+    return metadata;
+}
+
+export { autoNumberMetadata, checkboxMetadata };
